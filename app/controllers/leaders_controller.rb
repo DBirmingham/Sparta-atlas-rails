4352bc6fed023372fb9c6bf7d48a92c5986aae24
Tech-A03 <tech-a03@macbook-air-9.home>
2018-03-15 21:36:06 +0000

    was in the wrong folder when committing

--- app/controllers/leaders_controller.rb
+++ app/controllers/leaders_controller.rb
@@ -1,2 +1,38 @@
 class LeadersController < ApplicationController
+	def home
+		render :home
+	end
+	def index
+		@leader = Leader.all
+	end
+	def create
+		@leader = Leader.new(leader_params)
+		if(@leader.save)
+			redirect_to(leaders_path)
+		else
+			render action: :new
+		end
+	end
+	def new
+		@leader = Leader.new
+	end
+	def edit
+		@leader = Leader.find(params[:id].to_i)
+	end
+	def show
+		@leader = Leader.find(params[:id].to_i)
+	end
+	def update
+		@leader = Leader.find(params[:id].to_i)
+		@leader.update(leader_params)
+		redirect_to(leaders_path)
+	end
+	def destroy
+		@leader = Leader.find(params[:id].to_i)
+		@leader.destroy
+		redirect_to(leaders_path)
+	end
+	def leader_params
+		params.require(:leader).permit(:name, :age, :title)
+	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :leaders
+  resources :countries
+  resources :regions
+  root to: 'leaders#home'
 end
